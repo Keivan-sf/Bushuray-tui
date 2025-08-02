3f70fe463a7214a54ba08dfe45db403430ec8bb3
Keivan-sf <keyvan0082@gmail.com>
2025-08-02 20:29:00 +0330

    Make sure there is an english character after the config name
    For example if the config name is in persian/arabic and the
    terminal/shell is trying to support bidi, the order of the row would get
    messed up if the config name is followed by numbers or characters like
    '-',':',...

--- components/List/utils.go
+++ components/List/utils.go
@@ -9,9 +9,9 @@ import (
 func styleTestPrimary(ping int) string {
 	width := test_result_w
 	if ping > 0 {
-		return primary_style.Foreground(lipgloss.Color("#40a02b")).Width(width).MaxWidth(width).Render(strconv.Itoa(ping))
+		return primary_style.Foreground(lipgloss.Color("#40a02b")).Width(width).MaxWidth(width).Render("OK:" + strconv.Itoa(ping))
 	} else if ping == -1 {
-		return primary_style.Foreground(lipgloss.Color("#e64553")).Width(width).MaxWidth(width).Render(strconv.Itoa(ping))
+		return primary_style.Foreground(lipgloss.Color("#e64553")).Width(width).MaxWidth(width).Render("FAILED")
 	} else if ping == -2 {
 		return primary_style.Foreground(lipgloss.Color("#df8e1d")).Width(width).MaxWidth(width).Render("TESTING")
 	} else {
@@ -22,9 +22,9 @@ func styleTestPrimary(ping int) string {
 func styleTestUnderCursor(ping int) string {
 	width := test_result_w
 	if ping > 0 {
-		return under_cursor_style.Foreground(lipgloss.Color("#40a02b")).Width(width).MaxWidth(width).Render(strconv.Itoa(ping))
+		return under_cursor_style.Foreground(lipgloss.Color("#40a02b")).Width(width).MaxWidth(width).Render("OK:" + strconv.Itoa(ping))
 	} else if ping == -1 {
-		return under_cursor_style.Foreground(lipgloss.Color("#e64553")).Width(width).MaxWidth(width).Render(strconv.Itoa(ping))
+		return under_cursor_style.Foreground(lipgloss.Color("#e64553")).Width(width).MaxWidth(width).Render("FAILED")
 	} else if ping == -2 {
 		return under_cursor_style.Foreground(lipgloss.Color("#df8e1d")).Width(width).MaxWidth(width).Render("TESTING")
 	} else {
@@ -35,9 +35,9 @@ func styleTestUnderCursor(ping int) string {
 func styleTestNormal(ping int) string {
 	width := test_result_w
 	if ping > 0 {
-		return lipgloss.NewStyle().Foreground(lipgloss.Color("#40a02b")).Width(width).MaxWidth(width).Render(strconv.Itoa(ping))
+		return lipgloss.NewStyle().Foreground(lipgloss.Color("#40a02b")).Width(width).MaxWidth(width).Render("OK:" + strconv.Itoa(ping))
 	} else if ping == -1 {
-		return lipgloss.NewStyle().Foreground(lipgloss.Color("#e64553")).Width(width).MaxWidth(width).Render(strconv.Itoa(ping))
+		return lipgloss.NewStyle().Foreground(lipgloss.Color("#e64553")).Width(width).MaxWidth(width).Render("FAILED")
 	} else if ping == -2 {
 		return lipgloss.NewStyle().Foreground(lipgloss.Color("#df8e1d")).Width(width).MaxWidth(width).Render("TESTING")
 	} else {
