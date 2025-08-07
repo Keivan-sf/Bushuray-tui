aa14420677c747ddd259a04c390fecc0d474ad20
Keivan-sf <keyvan0082@gmail.com>
2025-08-07 13:09:57 +0330

    Improve tunview style

--- components/Tun/tunview.go
+++ components/Tun/tunview.go
@@ -3,6 +3,7 @@ package tunview
 import (
 	cmds "bushuray-tui/commands"
 	"bushuray-tui/utils"
+	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -17,13 +18,18 @@ func InitialModel() Model {
 	return Model{}
 }
 
+var sudo_bushuray = lipgloss.NewStyle().Foreground(lipgloss.Color("#ca9ee6")).Render("sudo bushuray")
+var dialog_text = fmt.Sprintf("Core is not running as root, do you want to kill it so you can start again with %s ?", sudo_bushuray)
+var dialog_style = lipgloss.NewStyle().Padding(0, 2).Align(lipgloss.Center)
+var help_style = lipgloss.NewStyle().MarginTop(2).Align(lipgloss.Center)
+var help_test = utils.GenHelp([]string{"enter", "esc"}, []string{"YES", "NO"})
+
 func (m Model) View() string {
-	dialog := lipgloss.NewStyle().Width(m.Width).MarginTop(2).Padding(0, 2).MaxWidth(m.Width).Align(lipgloss.Center).Render("Core is not running as root, do you want to kill it so you can start again with `sudo bushuray` ?")
-
-	help := lipgloss.NewStyle().Width(m.Width).MarginTop(2).MaxWidth(m.Width).Align(lipgloss.Center).Render(utils.GenHelp([]string{"enter", "esc"}, []string{"YES", "NO"}))
-
+	dialog := dialog_style.Width(m.Width).MaxWidth(m.Width).Render(dialog_text)
+	help := help_style.Width(m.Width).MaxWidth(m.Width).Render(help_test)
 	content := lipgloss.JoinVertical(lipgloss.Top, dialog, help)
-	return content
+	container := lipgloss.Place(m.Width, m.Height, lipgloss.Center, lipgloss.Center, content)
+	return container
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
@@ -41,6 +47,6 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 
 func (m Model) SetWH(width int, height int) Model {
 	m.Width = width
-	m.Height = width
+	m.Height = height
 	return m
 }
