919d538ac0967026c5e4713e18aca914c8c496e0
Keivan-sf <keyvan0082@gmail.com>
2025-08-10 02:11:34 +0330

    Render background for addgroup view

--- components/AddGroup/utils.go
+++ components/AddGroup/utils.go
@@ -1,19 +1,23 @@
 package addgroup
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"bushuray-tui/global"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func (m *Model) adjustToNewFocus(commands []tea.Cmd) []tea.Cmd {
 	for i := 0; i <= len(m.inputs)-1; i++ {
 		if i == m.focusIndex {
 			commands = append(commands, m.inputs[i].Focus())
-			m.inputs[i].PromptStyle = focusedStyle
-			m.inputs[i].TextStyle = focusedStyle
+			m.inputs[i].PromptStyle = focusedStyle.Background(global.GetBgColor())
+			m.inputs[i].TextStyle = focusedStyle.Background(global.GetBgColor())
 			continue
 		}
 		// Remove focused state
 		m.inputs[i].Blur()
-		m.inputs[i].PromptStyle = noStyle
-		m.inputs[i].TextStyle = noStyle
+		m.inputs[i].PromptStyle = noStyle.Background(global.GetBgColor())
+		m.inputs[i].TextStyle = noStyle.Background(global.GetBgColor())
 	}
 	return commands
 }
