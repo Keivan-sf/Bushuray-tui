e2c2f15d36f9b685b759e1bf4da9a0aff435c604
Keivan-sf <keyvan0082@gmail.com>
2025-08-10 01:43:37 +0330

    chore: improve method chaining clutter on tunview

--- components/Tun/tunview.go
+++ components/Tun/tunview.go
@@ -25,13 +25,39 @@ var help_test = shared.GenHelp([]string{"enter", "esc"}, []string{"YES", "NO"})
 
 func (m Model) View() string {
 	bg_style := lipgloss.NewStyle().Background(global.GetBgColor())
-	var sudo_bushuray = lipgloss.NewStyle().Foreground(lipgloss.Color("#ca9ee6")).Background(global.GetBgColor()).Render("sudo bushuray")
-	var dialog_text = lipgloss.JoinHorizontal(lipgloss.Left, bg_style.Render("Core is not running as root, do you want to kill it so you can start again with "), sudo_bushuray, bg_style.Render(" ?"))
-	dialog := dialog_style.Width(m.Width).MaxWidth(m.Width).Background(global.GetBgColor()).Render(dialog_text)
-	help := help_style.Width(m.Width).MaxWidth(m.Width).Background(global.GetBgColor()).Render(help_test)
+
+	sudo_bushuray := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ca9ee6")).
+		Background(global.GetBgColor()).
+		Render("sudo bushuray")
+
+	dialog_text := lipgloss.
+		JoinHorizontal(
+			lipgloss.Left,
+			bg_style.Render("Core is not running as root, do you want to kill it so you can start again with "),
+			sudo_bushuray,
+			bg_style.Render(" ?"))
+
+	dialog := dialog_style.
+		Width(m.Width).
+		MaxWidth(m.Width).
+		Background(global.GetBgColor()).
+		Render(dialog_text)
+
+	help := help_style.
+		Width(m.Width).
+		MaxWidth(m.Width).
+		Background(global.GetBgColor()).
+		Render(help_test)
+
 	content := lipgloss.JoinVertical(lipgloss.Top, dialog, help)
 	container := lipgloss.Place(m.Width, m.Height, lipgloss.Center, lipgloss.Center, content)
-	container_with_bg := lipgloss.NewStyle().Background(global.GetBgColor()).Render(container)
+
+	container_with_bg := lipgloss.
+		NewStyle().
+		Background(global.GetBgColor()).
+		Render(container)
+
 	return container_with_bg
 }
 
