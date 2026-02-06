f70ffb9ec93b09722ab6d03cf104800b2bb0c4d7
Keivan-sf <keyvan0082@gmail.com>
2026-02-06 16:10:00 +0330

    chore: bump version to `v0.1.8`

--- components/Tabs/apptitle.go
+++ components/Tabs/apptitle.go
@@ -8,7 +8,7 @@ import (
 
 func (m Model) renderAppTitle() string {
 	title := "Bushuray-tui"
-	version := "v0.1.7"
+	version := "v0.1.8"
 	title_text := lipgloss.NewStyle().Foreground(lipgloss.Color("#f5c2e7")).Background(global.GetBgColor()).Render(title)
 	separtor := lipgloss.NewStyle().Foreground(lipgloss.Color("#7287fd")).Background(global.GetBgColor()).Render(" • ")
 	version_text := lipgloss.NewStyle().Foreground(lipgloss.Color("#7287fd")).Background(global.GetBgColor()).Render(version)
